3a87a27bcb2be952e5dced12e365ff8907765af4
WithKKM <201800535@hufs.ac.kr>
2022-05-08 12:08:19 +0900

    python 기초

--- 문답딕셔너리.py
+++ 문답딕셔너리.py
@@ -1,14 +1,14 @@
-total_list = []
+total_dictionary = {}
 
 while True:
     question = input("질문을 입력해주세요 : ")
     if question == "q":
         break
     else:
-        total_list.append({"질문" : question, "답변" : ""})
+        total_dictionary[question] = ""
 
-for i in total_list:
-    print(i["질문"])
+for i in total_dictionary:
+    print(i)
     answer = input("답변을 입력해주세요 : ")
-    i["답변"] = answer
-print(total_list)
+    total_dictionary[i] = answer
+print(total_dictionary)
